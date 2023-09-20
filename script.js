7b4b3a0ee8462fd11eccea0a28dd6bb9c93030ac
Sorin05 <sorinandrei230587@gmail.com>
2023-09-20 10:35:14 +0100

    Added localstorage to save user data

--- script.js
+++ script.js
@@ -29,16 +29,18 @@ function lightMode(){
     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = 'Light Mode';
     toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    imageMode('light')
+    imageMode('light');
 }
 
 // Switch Theme Dynamically
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else {
-        document.documentElement.setAttribute('data-theme', 'light')
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 
@@ -46,3 +48,14 @@ function switchTheme(event) {
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check local stotage for theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
